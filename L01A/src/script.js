c391c6b235716fe6313bfd9768f023cfb22b289b
Naelly <naellyv61@gmail.com>
2026-07-29 21:59:03 -0300

    L01A

---
+++ L01A/src/script.js
@@ -0,0 +1,9 @@
+// Exercício 7A - Converter temperatura Celsius para Fahrenheit
+
+alert("Conversão de Celsius para Fahrenheit");
+
+let celsius = parseFloat(prompt("Informe a temperatura em Celsius: "));
+
+let fahrenheit = (9 * celsius + 160) / 5;
+
+alert("A temperatura em Fahrenheit é: " + fahrenheit);
