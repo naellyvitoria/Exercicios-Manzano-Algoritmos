a6a7aafa6b594ced019e299c7749b3fa1e74c94e
Naelly <naellyv61@gmail.com>
2026-07-30 22:28:27 -0300

    L01H

---
+++ L01H/src/script.js
@@ -0,0 +1,13 @@
+// Exercício 7H - Calcular volume de uma caixa retangular
+
+alert("Volume da caixa retangular");
+
+let comprimento = parseFloat(prompt("Informe o comprimento: "));
+
+let largura = parseFloat(prompt("Informe a largura: "));
+
+let altura = parseFloat(prompt("Informe a altura: "));
+
+let volume = comprimento * largura * altura;
+
+alert("O volume da caixa é: " + volume);
