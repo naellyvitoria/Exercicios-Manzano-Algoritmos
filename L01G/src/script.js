bbe29a049547eee2cf20173a1ba4a51fed9eb911
Naelly <naellyv61@gmail.com>
2026-07-30 22:19:58 -0300

    L01G

---
+++ L01G/src/script.js
@@ -0,0 +1,20 @@
+// Exercício 7G - Efetuar adição e multiplicação entre números
+
+alert("Operações matemáticas");
+
+let A = parseInt(prompt("Informe o valor de A: "));
+
+let B = parseInt(prompt("Informe o valor de B: "));
+
+let C = parseInt(prompt("Informe o valor de C: "));
+
+let D = parseInt(prompt("Informe o valor de D: "));
+
+alert(
+"A+B = " + (A+B) + " | A*B = " + (A*B) +
+"\nA+C = " + (A+C) + " | A*C = " + (A*C) +
+"\nA+D = " + (A+D) + " | A*D = " + (A*D) +
+"\nB+C = " + (B+C) + " | B*C = " + (B*C) +
+"\nB+D = " + (B+D) + " | B*D = " + (B*D) +
+"\nC+D = " + (C+D) + " | C*D = " + (C*D) 
+);
